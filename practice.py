85503256fe876864d9d6c1697d33710fe39773bf
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-05-30 18:58:57 +0900

    first_자료형

---
+++ practice.py
@@ -0,0 +1,17 @@
+from xmlrpc.client import boolean
+
+print('python 시작하기!!!')
+
+# 숫자 자료형
+# print(1) 정수
+# print(3.14) 실수
+
+# 문자 자료형
+# print('안녕!')
+# print('hello world')
+# print('0') 숫자여도 ''로 감싸게 된다면 문자 자료형
+
+# boolean 자료형
+# print(True)
+# print(False)
+
