023bb153a2e29f2c774d4de804f791d19f97426e
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-06-01 18:29:08 +0900

    변수

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from xmlrpc.client import boolean
 
 print('python 시작하기!!!')
@@ -15,3 +16,38 @@ print('python 시작하기!!!')
 # print(True)
 # print(False)
 
+
+# 변수
+# 어떤 값을 저장하는 공간
+# 변수 선언
+# 변수 이름 = 값
+
+# ex) 설날 세뱃돈!
+# 봉투 = 변수 / 세뱃돈 = 값
+# 변수 = 값
+envelope1 = 10000 # 초등학생
+envelope2 = 20000 # 고등학생
+envelope3 = '파이팅!!!' #직장인^^
+
+print(envelope1)
+print(envelope2)
+print(envelope3)
+
+# 변수 이름 규칙
+# 1. 문자 또는 _로 시작하기
+# ex) name, _name
+
+# 2. 문자, 숫자, _로 구성
+# ex) name123, _name_456
+
+# 3. 공백 안 됨, 특수문자 안 됨
+# ex) na me, @_name__^^~
+
+# 4. 대소문자 구분
+# ex) name, Name, Name -> 다 다른 변수로 구분합니다!
+
+# 5. 키워드(예약어) 사용 불가!!
+# ex) True, False, for, while 등
+
+# 6. 소문자 단어, _로 구분된 단어들(권장 사항!)
+# ex) name,my_name,your_name 등 밑줄로 구분된 단어들의 조합으로 만드는 것이 좋음!
