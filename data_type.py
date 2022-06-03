c6618f635598ab9405145a482f0c7ff393f38090
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-06-03 11:31:30 +0900

    형변환

---
+++ data_type.py
@@ -0,0 +1,15 @@
+print('python 시작하기!!!')
+# 자료형
+
+# 숫자 자료형
+# print(1) 정수
+# print(3.14) 실수
+
+# 문자 자료형
+# print('안녕!')
+# print('hello world')
+# print('0') 숫자여도 ''로 감싸게 된다면 문자 자료형
+
+# boolean 자료형
+# print(True)
+# print(Falsqe)
--- variable.py
+++ variable.py
@@ -1,23 +1,5 @@
-from unicodedata import name
-from xmlrpc.client import boolean
-
-print('python 시작하기!!!')
-
-# 숫자 자료형
-# print(1) 정수
-# print(3.14) 실수
-
-# 문자 자료형
-# print('안녕!')
-# print('hello world')
-# print('0') 숫자여도 ''로 감싸게 된다면 문자 자료형
-
-# boolean 자료형
-# print(True)
-# print(False)
-
-
 # 변수
+
 # 어떤 값을 저장하는 공간
 # 변수 선언
 # 변수 이름 = 값
