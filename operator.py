549d64182afa7b03834d3b1349900f1116c55d45
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-06-04 18:47:49 +0900

    operator

---
+++ operator.py
@@ -0,0 +1,73 @@
+# 연산자
+
+# + 더하기
+print(5+2)
+
+# - 빼기
+print(5-2)
+
+# * 곱하기
+print(5*2)
+
+# / 나누기
+print(5/2)
+
+# % 나머지
+print(5%2)
+
+# // 몫(나머지는 버림!)
+print(5//2)
+
+# ** 거듭 제곱
+print(5 ** 2)
+
+
+# 비교 연산자
+
+# > 크다
+print(5 > 2)
+
+# >= 크거나 같다
+print(5 >= 2)
+
+# < 작다
+print(5 < 2)
+
+# <= 작거나 같다
+print(5 <= 2)
+
+# == 같다
+print(5==2)
+
+# != 같지 않다
+print(5!=2)
+
+
+# 논리 연산자
+
+# and 둘다 참이면 True
+print(3<5 and 7<5)
+# >>False
+
+# or 하나라도 참이면 True
+print(3<5 or 7<5)
+# >>True
+
+# not 반전
+print(not 3<5)
+# >>False
+
+
+# 멤버 연산자
+
+# in 포함
+print('c' in 'cat')
+# >> True
+
+# not in 미포함
+print('c' not in 'cat')
+# >> False
+
+# 문제
+# A는 B보다 크거나 같고, C는 D보다 작거나 같다(파이썬 코드로 표현)
+# A>=B and C<=D
