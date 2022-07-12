fdd6e7a37f0102e4478e0d99ce42beab6e03fa9a
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-07-12 14:15:41 +0900

    while문

--- while.py
+++ while.py
@@ -35,3 +35,15 @@ d = 1
 while d <= c :
     print(d,d*d)
     d += 1
+
+
+# 코드 보고 실행 결과 맞히기
+number = 358
+
+rem = rev = 0 # rem = 0 , rev = 0
+while number >= 1:
+    rem = number % 10 # 1. rem = 8  2.rem = 5  3. rem = 3
+    rev = rev * 10 + rem # 1. rev = 8  2.rev = 85  3. rev = 853
+    number = number // 10 # 358/10 1. number = 35  2. number = 3  3. number = 0
+
+print(rev) # 853
