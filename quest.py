986f984d894a6b8ede83fdcc6dda2c4337bcd22f
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-08-10 13:30:47 +0900

    question

--- quest.py
+++ quest.py
@@ -20,13 +20,6 @@ while i <= 1000:
     i += 1
 print(n)
 
-result = 0
-i = 1
-while i <= 1000:
-    if i % 3 == 0: # 3으로 나누어 떨어지는 수는 3의 배수
-        result += i
-    i += 1
-print(result)
 
 # Q. while문 사용해 아래와 같이 *표시하는 프로그램 작성
 # *
@@ -34,6 +27,26 @@ print(result)
 # ***
 # ****
 # *****
+s = '*'
+nu = 1
+while nu <= 5 :
+    print(s*nu)
+    nu += 1
 
 
 # Q. for문 사용해 1부터 100까지 숫자 출력
+for i in range(1,101):
+    print(f'{i}')
+
+
+# Q. 총 10명의 학생, for문 사용해 학급의 평균 점수 구하기
+#   [70,60,55,75,95,90,80,80,85,100]
+
+
+# Q. 아래 내용은 리스트 중에 홀수에만 2를 곱해 저장하는 코드
+numbers = [1,2,3,4,5]
+result1 = []
+for n in numbers:
+    if n % 2 == 1:
+        result1.append(n*2)
+# 위 코드를 리스트 내포를 사용하여 표현하기
